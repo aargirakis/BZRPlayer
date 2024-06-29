63dcbb17e48deaba1df4c49dbd9ec20e72d00a01
Ciro Scognamiglio <>
2024-06-29 10:24:26 +0200

    fix: bzr2 exe linux cross-compilation (wine target)

--- docker/run.sh
+++ docker/run.sh
@@ -4,14 +4,18 @@ CONTAINER_NAME="toolchain"
 IMAGE_NAME="toolchain:latest"
 BUILD_TYPE="Release"
 BUILD_DIR=$(echo "/home/devel/bzr2/cmake-build-docker-$BUILD_TYPE" | tr '[:upper:]' '[:lower:]')
-COMMAND="su devel -c 'mkdir -p $BUILD_DIR && cd $BUILD_DIR && i686-w64-mingw32-cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -G Ninja .. && ninja'"
-#COMMAND="sleep 1000"
-# TODO stop container if running (and delete it?)
-# TODO x86_64-w64-mingw32-cmake
-# TODO avoid updating cmake if cmake local or repo ver is higher than 3.29
+COMMAND="su devel -c 'mkdir -p $BUILD_DIR && cd $BUILD_DIR && i686-w64-mingw32-cmake \
+-DCMAKE_PREFIX_PATH=/usr/i686-w64-mingw32 -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCPACK_EXECUTABLE=/usr/bin/cpack \
+-G Ninja .. && ninja'"
 
-if [ -z "$(docker images -q $IMAGE_NAME 2>/dev/null)" ]; then
-  docker build -t $IMAGE_NAME .
+# TODO allow external profile set
+
+if [ -z "$(docker images -q "$IMAGE_NAME" 2>/dev/null)" ]; then
+  docker build -t "$IMAGE_NAME" .
+fi
+
+if [ "$(docker ps -a -q -f name="$CONTAINER_NAME")" ]; then
+  docker rm -f "$CONTAINER_NAME"
 fi
 
 docker run --rm -v "$(realpath ../):/home/devel/bzr2" --name "$CONTAINER_NAME" "$IMAGE_NAME" bash -c "$COMMAND"
