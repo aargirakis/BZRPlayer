11d5ee8f0f96a8d275e96591a6f2de3acb0451b2
Ciro Scognamiglio <>
2024-07-28 17:35:56 +0200

    feat: repo clone&patch (plugin_libopenmpt)

--- src/plugins/FMOD_libopenmpt_plugin/src/libopenmpt-0.7.3+release/build/dist.mk
+++
@@ -1,4 +0,0 @@
-
-MPT_SVNVERSION=19618
-MPT_SVNURL=https://source.openmpt.org/svn/openmpt/tags/libopenmpt-0.7.3
-MPT_SVNDATE=2023-09-09T11:29:35.564177Z
--- src/plugins/FMOD_libopenmpt_plugin/src/libopenmpt-0.7.6+release/build/dist.mk
+++
@@ -1,4 +0,0 @@
-
-MPT_SVNVERSION=20445
-MPT_SVNURL=https://source.openmpt.org/svn/openmpt/tags/libopenmpt-0.7.6
-MPT_SVNDATE=2024-03-24T12:17:57.924136Z
