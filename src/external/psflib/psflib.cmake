7070a7643a0c54ae938583fc055278106a9363f7
Ciro Scognamiglio <>
2024-08-11 19:55:02 +0200

    refactor: avoid deprecated FetchContent_Populate

--- src/external/psflib/psflib.cmake
+++ src/external/psflib/psflib.cmake
@@ -1,20 +1,18 @@
 cmake_minimum_required(VERSION 3.28)
 
-set(GIT_REPO_NAME "psflib")
-set(GIT_REPO_URL "https://gitlab.com/kode54/${GIT_REPO_NAME}.git")
-set(GIT_CLONE_AT "3bea757c8b45c5e68da1b5a7b736ad960a06a124")
-set(GIT_IS_TAG false)
-
-FetchContent_GetProperties(${GIT_REPO_NAME})
-
-if (NOT ${GIT_REPO_NAME}_POPULATED)
-    clone_and_patch(
-            ${GIT_REPO_NAME} ${GIT_REPO_URL} ${GIT_CLONE_AT} ${GIT_IS_TAG}
+set(LIB_NAME "psflib")
+set(LIB_VERSION "3bea757c8b45c5e68da1b5a7b736ad960a06a124")
+set(LIB_NAME_VERSIONED "${LIB_NAME}-${LIB_VERSION}")
+set(LIB_FILENAME "${LIB_NAME_VERSIONED}.zip")
+set(LIB_URL "https://gitlab.com/kode54/${LIB_NAME}/-/archive/${LIB_VERSION}/${LIB_FILENAME}")
+set(LIB_SHA_256_HASH "16cf2ffc3113c84edcb04e2980087626c70e1915c9597eeb0a5f70b36d30d1fd")
+if (NOT TARGET "${LIB_NAME}_ExternalProject_Add")
+    download_and_patch(
+            ${LIB_NAME} ${LIB_NAME_VERSIONED} ${LIB_FILENAME} ${LIB_URL} ${LIB_SHA_256_HASH}
             ""
             ""
     )
 
-    set(${GIT_REPO_NAME}_POPULATED true)
-    set(EXTERNAL_SOURCE_DIR_${GIT_REPO_NAME} "${EXTERNAL_SOURCE_DIR}")
-    set(EXTERNAL_SOURCE_DIR_${GIT_REPO_NAME} "${EXTERNAL_SOURCE_DIR}" PARENT_SCOPE)
+    set(EXTERNAL_SOURCE_DIR_${LIB_NAME} "${EXTERNAL_SOURCE_DIR}")
+    set(EXTERNAL_SOURCE_DIR_${LIB_NAME} "${EXTERNAL_SOURCE_DIR}" PARENT_SCOPE)
 endif ()
--- utils.cmake
+++ utils.cmake
@@ -1,3 +1,6 @@
+include(FetchContent)
+include(ExternalProject)
+
 function(patch_sources target_name external_source_dir external_sources_to_remove patch_sources_dir)
     if (NOT external_sources_to_remove AND NOT patch_sources_dir)
         return()
@@ -27,41 +30,11 @@ function(patch_sources target_name external_source_dir external_sources_to_remov
     endif ()
 endfunction()
 
-function(clone_and_patch repo_name repo_url repo_tag is_tag external_sources_to_remove patch_sources_dir)
-    if (is_tag)
-        set(clone_at tag)
-    else ()
-        set(clone_at commit)
-    endif ()
-
-    message(STATUS "Cloning repository '${repo_name}' ${repo_url} at ${clone_at} '${repo_tag}'")
-
-    include(FetchContent)
-
-    #set(PATCH_FILE git apply ${CMAKE_CURRENT_SOURCE_DIR}/patches/test.patch)
-    FetchContent_Declare(
-            ${repo_name}
-            GIT_REPOSITORY ${repo_url}
-            GIT_TAG ${repo_tag}
-            GIT_SHALLOW ${is_tag}
-            # PATCH_COMMAND ${PATCH_FILE}
-    )
-
-    FetchContent_Populate(${repo_name})
-
-    set(external_source_dir ${${repo_name}_SOURCE_DIR})
-
-    patch_sources("${repo_name}" "${external_source_dir}" "${external_sources_to_remove}" "${patch_sources_dir}")
-
-    set(EXTERNAL_SOURCE_DIR ${external_source_dir} PARENT_SCOPE)
-endfunction()
-
-function(download_and_patch target_name target_name_versioned target_filename target_url sha_256_hash external_sources_to_remove patch_sources_dir)
+function(download_patch_and_make target_name target_name_versioned target_filename target_url sha_256_hash external_sources_to_remove patch_sources_dir make_args)
     message(STATUS "Downloading '${target_name_versioned}' at '${target_url}'")
 
-    set(external_source_dir "${CMAKE_BINARY_DIR}/${target_name_versioned}")
-
-    set(downloaded_file "${CMAKE_BINARY_DIR}/${target_name_versioned}/${target_filename}")
+    set(external_source_dir "${CMAKE_BINARY_DIR}/_deps/${target_name_versioned}")
+    set(downloaded_file "${external_source_dir}/${target_filename}")
 
     file(
             DOWNLOAD ${target_url} ${downloaded_file}
@@ -77,7 +50,8 @@ function(download_and_patch target_name target_name_versioned target_filename ta
     endif ()
 
     execute_process(
-            COMMAND ${CMAKE_COMMAND} -E tar xjf ${downloaded_file} #TODO? --directory ${UNTAR_DESTINATION}
+            COMMAND ${CMAKE_COMMAND} -E tar xjf ${downloaded_file}
+            WORKING_DIRECTORY ${external_source_dir}/..
             RESULT_VARIABLE UNPACK_RESULT
     )
 
@@ -85,18 +59,50 @@ function(download_and_patch target_name target_name_versioned target_filename ta
         message(FATAL_ERROR "Error unpacking '${downloaded_file}'")
     endif ()
 
-    include(FetchContent)
-
-    FetchContent_Declare(
-            ${target_name}
-            SOURCE_DIR ${external_source_dir}
-    )
-
-    FetchContent_Populate(${target_name})
-
     patch_sources("${target_name_versioned}" "${external_source_dir}" "${external_sources_to_remove}" "${patch_sources_dir}")
 
+    if (make_args)
+        if (CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
+            set(make_command
+                    ${CMAKE_PREFIX_PATH}/../../Tools/mingw810_32/bin/mingw32-make.exe
+                    AR=${CMAKE_PREFIX_PATH}/../../Tools/mingw810_32/bin/ar.exe
+            )
+        else ()
+            set(make_command make)
+        endif ()
+
+        set(make_command ${make_command} -j -l10 ${make_args}) #TODO -j -l10?
+    endif ()
+
+        ExternalProject_Add(  #TODO add update step & patch step with ExternalProject_Add
+                ${target_name}_ExternalProject_Add
+                #URL ${target_url}
+                #URL_HASH SHA256=${sha_256_hash}
+                SOURCE_DIR ${external_source_dir}
+                DOWNLOAD_COMMAND ""
+                #PATCH_COMMAND ""
+                CONFIGURE_COMMAND ""
+                BUILD_COMMAND "${make_command}"
+                INSTALL_COMMAND ""
+                WORKING_DIRECTORY ${external_source_dir}
+                #DOWNLOAD_NO_PROGRESS true
+                #CONFIGURE_HANDLED_BY_BUILD true
+                BUILD_IN_SOURCE 1
+                #BUILD_ALWAYS OFF
+                #EXCLUDE_FROM_ALL #TODO instead of external_sources_to_remove
+                #TODO skip download and extraction when not needed
+        )
+
     set(EXTERNAL_SOURCE_DIR ${external_source_dir} PARENT_SCOPE)
+endfunction()
+
+function(download_and_patch target_name target_name_versioned target_filename target_url sha_256_hash external_sources_to_remove patch_sources_dir)
+    download_patch_and_make(
+            "${target_name}" "${target_name_versioned}" "${target_filename}" "${target_url}" "${sha_256_hash}"
+            "${external_sources_to_remove}" "${patch_sources_dir}" ""
+    )
+
+    set(EXTERNAL_SOURCE_DIR ${EXTERNAL_SOURCE_DIR} PARENT_SCOPE)
 endfunction()
 
 function(download_to target_filename target_url sha_256_hash destination_path)
@@ -122,4 +128,32 @@ function(download_to target_filename target_url sha_256_hash destination_path)
         message(FATAL_ERROR "Error downloading '${target_filename}': ${ERROR_MESSAGE}")
     endif ()
 
+endfunction()
+
+#TODO remove (unused & deprecated)
+function(clone_and_patch repo_name repo_url repo_tag is_tag external_sources_to_remove patch_sources_dir)
+    if (is_tag)
+        set(clone_at tag)
+    else ()
+        set(clone_at commit)
+    endif ()
+
+    message(STATUS "Cloning repository '${repo_name}' ${repo_url} at ${clone_at} '${repo_tag}'")
+
+    #set(PATCH_FILE git apply ${CMAKE_CURRENT_SOURCE_DIR}/patches/test.patch)
+    FetchContent_Declare(
+            ${repo_name}
+            GIT_REPOSITORY ${repo_url}
+            GIT_TAG ${repo_tag}
+            GIT_SHALLOW ${is_tag}
+            # PATCH_COMMAND ${PATCH_FILE}
+    )
+
+    FetchContent_Populate(${repo_name})
+
+    set(external_source_dir ${${repo_name}_SOURCE_DIR})
+
+    patch_sources("${repo_name}" "${external_source_dir}" "${external_sources_to_remove}" "${patch_sources_dir}")
+
+    set(EXTERNAL_SOURCE_DIR ${external_source_dir} PARENT_SCOPE)
 endfunction()
