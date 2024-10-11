c91886d33845b4391cf4a58debafb59287bcee1f
Ciro Scognamiglio <>
2024-10-11 16:55:15 +0200

    fmod: improve dll copy

--- src/external/fmod/fmod.cmake
+++ src/external/fmod/fmod.cmake
@@ -7,3 +7,11 @@ set(LIB_FILENAME "FMOD SoundSystem ${LIB_VERSION}.zip")
 unpack_and_patch(${CMAKE_CURRENT_LIST_DIR}/dist/${LIB_FILENAME} ${LIB_NAME_VERSIONED} true "" ${CMAKE_CURRENT_LIST_DIR}/patches)
 
 set(EXTERNAL_SOURCE_DIR_${LIB_NAME} "${EXTERNAL_SOURCE_DIR}/FMOD SoundSystem/FMOD Studio API Windows")
+
+add_custom_target(copy-fmod-lib ALL
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+        ${EXTERNAL_SOURCE_DIR_${LIB_NAME}}/api/core/lib/x86/fmod.dll
+        ${OUTPUT_DIR}
+        VERBATIM
+)
+ 
