813a9af44966ebe52517cb8dacf0b6cea27416c7
Ciro Scognamiglio <>
2024-10-11 18:09:41 +0200

    cleanup

--- src/external/fmod/fmod.cmake
+++ src/external/fmod/fmod.cmake
@@ -8,7 +8,8 @@ unpack_and_patch(${CMAKE_CURRENT_LIST_DIR}/dist/${LIB_FILENAME} ${LIB_NAME_VERSI
 
 set(EXTERNAL_SOURCE_DIR_${LIB_NAME} "${EXTERNAL_SOURCE_DIR}/FMOD SoundSystem/FMOD Studio API Windows")
 
-add_custom_target(copy-fmod-lib ALL
+add_custom_target(
+        copy-lib-${LIB_NAME} ALL
         COMMAND ${CMAKE_COMMAND} -E copy_if_different
         ${EXTERNAL_SOURCE_DIR_${LIB_NAME}}/api/core/lib/x86/fmod.dll
         ${OUTPUT_DIR}
