22dce43fd7bec06c5717fb9aabaf9f8d3a8f7cab
Ciro Scognamiglio <>
2024-10-17 09:27:52 +0200

    refactor baserow

--- src/app/BaseRow.h
+++ src/app/BaseRow.h
@@ -4,15 +4,15 @@
 class BaseRow {
 public:
 	BaseRow();
-	int step;
-	int note;
-	int sample;
 	int effect;
 	int effect2; //for ahx/hively pattern view
+	int note;
 	int param;
 	int param2; //for ahx/hively pattern view
-    int vol; //for s3m pattern view
+	int sample;
 	int speed; //remove this for flod 5 version
+	int step;
+	int vol; //for s3m pattern view
     std::string noteText;
 };
 #endif // BASEROW_H
