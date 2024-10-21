2e10aa03a1d49c53802418d9e67b907be9c971fb
Andreas Argirakis <andreas@blazer.nu>
2024-10-21 10:32:12 +0200

    chore(ui): remove "name" & "value" headers in info window (--542)

--- src/app/mainwindow.cpp
+++ src/app/mainwindow.cpp
@@ -302,6 +302,7 @@ MainWindow::MainWindow(int argc, char *argv[], QWidget *parent) : QMainWindow(pa
     ui->tableInfo->setFont(roboto);
 
     QStringList columnLabelsInfo;
+    ui->tableInfo->horizontalHeader()->hide();
     columnLabelsInfo << "Name" << "Value";
     ui->tableInfo->setColumnCount(columnLabelsInfo.size());
     ui->tableInfo->setHorizontalHeaderLabels(columnLabelsInfo);
