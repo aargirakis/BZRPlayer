13e405c919414a0e36c3dbd29167e4d1aaa30dcf
Ciro Scognamiglio <>
2025-03-28 08:14:32 +0100

    build(qt-ads): update to 4.4.0

--- src/external/qt-ads/qt-ads.cmake
+++ src/external/qt-ads/qt-ads.cmake
@@ -3,13 +3,13 @@ cmake_minimum_required(VERSION 3.28)
 set(LIB_NAME "qt-ads")
 set(LIB_NAME2 "Qt-Advanced-Docking-System")
 string(TOLOWER ${LIB_NAME2} LIB_NAME3)
-set(LIB_VERSION "4.3.1")
+set(LIB_VERSION "4.4.0")
 set(LIB_NAME_VERSIONED "${LIB_NAME}-${LIB_VERSION}")
 set(LIB_NAME_VERSIONED2 "${LIB_NAME2}-${LIB_VERSION}")
 set(LIB_FILENAME "${LIB_NAME_VERSIONED2}.zip")
 set(LIB_FILENAME_URL "${LIB_VERSION}.zip")
 set(LIB_URL "https://github.com/githubuser0xFFFF/${LIB_NAME3}/archive/refs/tags/${LIB_FILENAME_URL}")
-set(LIB_SHA_256_HASH "40d37227eb88d4f88f0eb918cdba60959d6cb9cb52796fe156565a2d7cc2a146")
+set(LIB_SHA_256_HASH "06984d3d80cf714d825d29e092a704bfeb59a871e1bc1089be329f0fbc43f892")
 set(LIB_UNPACKED_DIR "${LIB_NAME_VERSIONED2}")
 set(ADS_VERSION ${LIB_VERSION})
 set(CMAKE_BUILD_TYPE "${CMAKE_BUILD_TYPE}")
@@ -18,16 +18,17 @@ download_patch_and_cmake(
         ${CMAKE_CURRENT_LIST_DIR}/patches
 )
 
-set_target_properties(qt5advanceddocking PROPERTIES
+set_target_properties(qtadvanceddocking-qt${QT_VERSION_MAJOR} PROPERTIES
         ARCHIVE_OUTPUT_DIRECTORY ""
         LIBRARY_OUTPUT_DIRECTORY ""
         RUNTIME_OUTPUT_DIRECTORY "${OUTPUT_DIR}"
 )
 
 if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-    set(LIB_QT_ADVANCED_DOCKING_SYSTEM "${OUTPUT_DIR}/libqt5advanceddockingd.dll")
+    set(LIB_QT_ADVANCED_DOCKING_SYSTEM "${OUTPUT_DIR}/libqtadvanceddocking-qt${QT_VERSION_MAJOR}d.dll")
 else ()
-    set(LIB_QT_ADVANCED_DOCKING_SYSTEM "${OUTPUT_DIR}/libqt5advanceddocking.dll")
+    set(LIB_QT_ADVANCED_DOCKING_SYSTEM "${OUTPUT_DIR}/libqtadvanceddocking-qt${QT_VERSION_MAJOR}.dll")
 endif ()
 
+set(LIB_QT_ADVANCED_DOCKING_SYSTEM ${LIB_QT_ADVANCED_DOCKING_SYSTEM} PARENT_SCOPE)
 set(EXTERNAL_SOURCE_DIR_${LIB_NAME} "${EXTERNAL_SOURCE_DIR}")
