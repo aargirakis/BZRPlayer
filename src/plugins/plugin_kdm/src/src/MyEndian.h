446c343d0f8020261e22c099af726d5a2fa1afd2
Ciro Scognamiglio <>
2025-04-10 22:07:13 +0200

    wip

--- src/plugins/plugin_kdm/src/src/MyEndian.h
+++ src/plugins/plugin_kdm/src/src/MyEndian.h
@@ -8,7 +8,7 @@
 
 // Convert high bytes and low bytes of MSW and LSW to 32-bit word.
 // Used to read 32-bit words in little-endian order.
-inline uint32_t readEndian(u_int8_t hihi, u_int8_t hilo, u_int8_t hi, u_int8_t lo)
+inline uint32_t readEndian(uint8_t hihi, uint8_t hilo, uint8_t hi, uint8_t lo)
 {
   return(( (uint32_t)hihi << 24 ) + ( (uint32_t)hilo << 16 ) +
 		 ( (uint32_t)hi << 8 ) + (uint32_t)lo );
