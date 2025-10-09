0d8b31d673c4b1c7ad9daeda8325ce7bc984dfc0
Ciro Scognamiglio <ciro.scognamiglio88@gmail.com>
2025-10-09 17:17:18 +0200

    build(zxtune): update to 5100

--- src/plugins/plugin_zxtune/src/main.cpp
+++ src/plugins/plugin_zxtune/src/main.cpp
@@ -1,18 +1,80 @@
 #include <cstring>
-#include "apps/libzxtune/zxtune.h"
 #include <iostream>
 #include <string>
+#include <attributes.h>
+#include <binary/container_factories.h>
+#include <core/data_location.h>
+#include <core/service.h>
+#include <error.h>
+#include <parameters/container.h>
+#include <module/holder.h>
+#include <module/players/pipeline.h>
 #include "info.h"
 #include "fmod_errors.h"
 #include "plugins.h"
 
 using namespace std;
 
+Binary::Container::Ptr CreateData(FMOD_CODEC_STATE *codec) {
+    unsigned int filesize;
+    unsigned int bytesread;
+    FMOD_CODEC_FILE_SIZE(codec, &filesize);
+    if (filesize == 4294967295) //stream
+    {
+        return nullptr;
+    }
+
+    //rewind file pointer
+    FMOD_RESULT result = FMOD_CODEC_FILE_SEEK(codec, 0, 0);
+
+    if (result != FMOD_OK) {
+        return nullptr;
+    }
+
+    /* Allocate space for buffer. */
+    auto myBuffer = std::make_unique<Binary::Dump>(filesize);
+
+    //read whole file to memory
+    result = FMOD_CODEC_FILE_READ(codec, myBuffer->data(), filesize, &bytesread);
+    if (result != FMOD_OK) {
+        return nullptr;
+    }
+    myBuffer->resize(bytesread);
+    return Binary::CreateContainer(std::move(myBuffer));
+}
+
+const ZXTune::Service &GetService() {
+    static const auto service = ZXTune::Service::Create(Parameters::Container::Create());
+    return *service;
+}
+
+class LightweightBinaryContainer : public Binary::Container {
+public:
+    LightweightBinaryContainer(const uint8_t *data, std::size_t size) : RawData(data), RawSize(size) {
+    }
+
+    [[nodiscard]] const void *Start() const override {
+        return RawData;
+    }
+
+    [[nodiscard]] std::size_t Size() const override {
+        return RawSize;
+    }
+
+    [[nodiscard]] Ptr GetSubcontainer(std::size_t offset, std::size_t size) const override {
+        auto copy(std::make_unique<Binary::Dump>(RawData + offset, RawData + std::min(RawSize, offset + size)));
+        return Binary::CreateContainer(std::move(copy));
+    }
+
+private:
+    const uint8_t *const RawData;
+    const std::size_t RawSize;
+};
+
 static FMOD_RESULT F_CALL open(FMOD_CODEC_STATE *codec, FMOD_MODE usermode, FMOD_CREATESOUNDEXINFO *userexinfo);
 static FMOD_RESULT F_CALL close(FMOD_CODEC_STATE *codec);
 static FMOD_RESULT F_CALL read(FMOD_CODEC_STATE *codec, void *buffer, unsigned int size, unsigned int *read);
 static FMOD_RESULT F_CALL setPosition(FMOD_CODEC_STATE *codec, int subsound, unsigned int position, FMOD_TIMEUNIT postype);
-static FMOD_RESULT F_CALL getPosition(FMOD_CODEC_STATE *codec, unsigned int *position, FMOD_TIMEUNIT postype);
 
 FMOD_CODEC_DESCRIPTION codecDescription =
 {
@@ -48,12 +110,12 @@ public:
         //delete some stuff
     }
 
-    ZXTuneHandle data;
-    ZXTuneHandle module;
-    ZXTuneHandle player;
+    Module::Holder::Ptr module;
+    Module::Renderer::Ptr renderer;
     Info* info;
-    int posAfterSeek;
     FMOD_CODEC_WAVEFORMAT waveformat;
+    Sound::Chunk chunk;
+    unsigned int chunkSamplesBuffered = 0;
 };
 
 /*
@@ -76,272 +138,242 @@ F_EXPORT FMOD_CODEC_DESCRIPTION* F_CALL FMODGetCodecDescription()
 
 static FMOD_RESULT F_CALL open(FMOD_CODEC_STATE* codec, FMOD_MODE usermode, FMOD_CREATESOUNDEXINFO* userexinfo)
 {
-    auto* plugin = new pluginZxtune(codec);
-    plugin->info = static_cast<Info*>(userexinfo->userdata);
+    try {
+        auto* plugin = new pluginZxtune(codec);
+        plugin->info = static_cast<Info*>(userexinfo->userdata);
 
-    unsigned int bytesread;
-    unsigned int filesize;
-    FMOD_CODEC_FILE_SIZE(codec, &filesize);
-    if (filesize == 4294967295) //stream
-    {
+        const Binary::Container::Ptr dataContainer = CreateData(codec);
+
+        if (!dataContainer) {
+            delete plugin;
+            return FMOD_ERR_FORMAT;
+        }
+
+        plugin->module = GetService().OpenModule(dataContainer, "", Parameters::Container::Create());
+
+        plugin->waveformat.format = FMOD_SOUND_FORMAT_PCM16;
+        plugin->waveformat.channels = 2;
+        plugin->waveformat.frequency = 44100;
+        plugin->waveformat.pcmblocksize = plugin->waveformat.format * plugin->waveformat.channels;
+
+        auto durationMs = plugin->module->GetModuleInformation()->Duration().Get();
+        plugin->waveformat.lengthpcm = durationMs / 1000 * plugin->waveformat.frequency;
+
+        codec->waveformat = &(plugin->waveformat);
+        codec->numsubsounds = 0;
+        /* number of 'subsounds' in this sound.  For most codecs this is 0, only multi sound codecs such as FSB or CDDA have subsounds. */
+        codec->plugindata = plugin; /* user data value */
+
+        // handle plugin preferences here
+        auto params = Parameters::Container::Create();
+
+        plugin->renderer = CreatePipelinedRenderer(*plugin->module, params);
+
+        const Parameters::Accessor::Ptr moduleProperties = plugin->module->GetModuleProperties();
+
+        // plugin->info->numChannels = zxinfo.Channels;
+        // plugin->info->numOrders = zxinfo.Positions;
+        // plugin->info->loopPosition = zxinfo.LoopPosition;
+        // plugin->info->loopFrame = zxinfo.LoopFrame;
+        // plugin->info->numPatterns = zxinfo.Patterns;
+        // plugin->info->numFrames = zxinfo.Frames;
+        plugin->info->initialTempo = 100;
+        Parameters::FindValue(*moduleProperties, Module::ATTR_TITLE, plugin->info->title);
+        Parameters::FindValue(*moduleProperties, Module::ATTR_AUTHOR, plugin->info->author);
+        Parameters::FindValue(*moduleProperties, Module::ATTR_PROGRAM, plugin->info->replay);
+        Parameters::FindValue(*moduleProperties, Module::ATTR_COMMENT, plugin->info->comments);
+
+        std::string type;
+        Parameters::FindValue(*moduleProperties, Module::ATTR_TYPE, type);
+
+        if (type == "AS0")
+        {
+            plugin->info->fileformat = "ASC Sound Master v0.xx";
+        }
+        else if (type == "AY")
+        {
+            plugin->info->fileformat = "AY ZX Spectrum/Amstrad CPC";
+        }
+        else if (type == "AYC")
+        {
+            plugin->info->fileformat = "AYC Amstrad CPC";
+        }
+        else if (type == "ASC")
+        {
+            plugin->info->fileformat = "ASC Sound Master v1.xx-2.xx";
+        }
+        else if (type == "ET1")
+        {
+            plugin->info->fileformat = "Extreme Tracker v1.x";
+        }
+        else if (type == "FTC")
+        {
+            plugin->info->fileformat = "Spectrum Fast Tracker";
+        }
+        else if (type == "GTR")
+        {
+            plugin->info->fileformat = "Global Tracker";
+        }
+        else if (type == "MTC")
+        {
+            plugin->info->fileformat = "Multitrack Container";
+        }
+        else if (type == "PSC")
+        {
+            plugin->info->fileformat = "Pro Sound Creator";
+        }
+        else if (type == "PSG")
+        {
+            plugin->info->fileformat = "Spectrum PSG";
+        }
+        else if (type == "PSM")
+        {
+            plugin->info->fileformat = "Pro Sound Maker";
+        }
+        else if (type == "PT1")
+        {
+            plugin->info->fileformat = "Spectrum Pro Tracker 1";
+        }
+        else if (type == "PT2")
+        {
+            plugin->info->fileformat = "Spectrum Pro Tracker 2";
+        }
+        else if (type == "PT3")
+        {
+            plugin->info->fileformat = "Spectrum Pro Tracker 3";
+        }
+        else if (type == "SQT")
+        {
+            plugin->info->fileformat = "SQ Tracker";
+        }
+        else if (type == "ST1")
+        {
+            plugin->info->fileformat = "Spectrum Sound Tracker 1";
+        }
+        else if (type == "ST3")
+        {
+            plugin->info->fileformat = "Spectrum Sound Tracker 3";
+        }
+        else if (type == "STC")
+        {
+            plugin->info->fileformat = "Compiled Spectrum Sound Tracker 1";
+        }
+        else if (type == "STP")
+        {
+            plugin->info->fileformat = "Spectrum Sound Tracker Pro 1";
+        }
+        else if (type == "TXT")
+        {
+            plugin->info->fileformat = "Vortextracker (Pro Tracker 3)";
+        }
+        else if (type == "TS")
+        {
+            plugin->info->fileformat = "TurboSound module for AY Emulator";
+        }
+        else if (type == "VTX")
+        {
+            plugin->info->fileformat = "Vortextracker";
+        }
+        else if (type == "YM") //played by st-sound
+        {
+            plugin->info->fileformat = "YM";
+        }
+        else if (type == "STR")
+        {
+            plugin->info->fileformat = "Sample Tracker";
+        }
+        else if (type == "CHI")
+        {
+            plugin->info->fileformat = "Spectrum Chip Tracker";
+        }
+        else if (type == "SQD")
+        {
+            plugin->info->fileformat = "SG Digital Tracker";
+        }
+        else if (type == "DMM")
+        {
+            plugin->info->fileformat = "Digital Music Maker";
+        }
+        else if (type == "PDT")
+        {
+            plugin->info->fileformat = "Prodigi Tracker";
+        }
+        else if (type == "DST")
+        {
+            plugin->info->fileformat = "Digital Studio for AY and Covox";
+        }
+        else if (type == "COP")
+        {
+            plugin->info->fileformat = "Sam Coupe E-Tracker";
+        }
+        else if (type == "TFE")
+        {
+            plugin->info->fileformat = "TFM Music Maker 1.3+";
+        }
+        else if (type == "TF0")
+        {
+            plugin->info->fileformat = "TFM Music Maker 0.1-1.2";
+        }
+        else if (type == "TFD")
+        {
+            plugin->info->fileformat = "TurboFM Dumped";
+        }
+        else if (type == "TFC")
+        {
+            plugin->info->fileformat = "TurboFM Compiled";
+        }
+        else
+        {
+            plugin->info->fileformat = "Unknown ZXTune";
+            //plugin->info->fileformat =ZXTune_GetInfo(plugin->player,"Program")+"("+type+")";
+        }
+
+        plugin->info->plugin = PLUGIN_zxtune;
+        plugin->info->pluginName = PLUGIN_zxtune_NAME;
+        plugin->info->setSeekable(true);
+
+        return FMOD_OK;
+    } catch (const Error &error) {
+        cout << "ZXTune: " << error.ToString() << endl;
         return FMOD_ERR_FORMAT;
     }
-
-    /* Allocate space for buffer. */
-    auto myBuffer = new signed short[filesize];
-
-    //rewind file pointer
-    FMOD_RESULT result = FMOD_CODEC_FILE_SEEK(codec, 0, 0);
-
-    if (result != FMOD_OK)
-    {
-        return FMOD_ERR_FORMAT;
-    }
-
-    //read whole file to memory
-    result = FMOD_CODEC_FILE_READ(codec, myBuffer, filesize, &bytesread);
-    if (result != FMOD_OK)
-    {
-        return FMOD_ERR_FORMAT;
-    }
-
-    plugin->waveformat.format = FMOD_SOUND_FORMAT_PCM16;
-    plugin->waveformat.channels = 2;
-    plugin->waveformat.frequency = 44100;
-    plugin->waveformat.pcmblocksize = (16 >> 3) * plugin->waveformat.channels;
-
-    codec->waveformat = &(plugin->waveformat);
-    codec->numsubsounds = 0;
-    /* number of 'subsounds' in this sound.  For most codecs this is 0, only multi sound codecs such as FSB or CDDA have subsounds. */
-    codec->plugindata = plugin; /* user data value */
-
-    plugin->data = ZXTune_CreateData(myBuffer, filesize);
-
-    delete [] myBuffer;
-
-    plugin->module = ZXTune_OpenModule(plugin->data);
-    plugin->player = ZXTune_CreatePlayer(plugin->module);
-
-    if (!plugin->player)
-    {
-        return FMOD_ERR_FORMAT;
-    }
-
-    ZXTuneModuleInfo zxinfo;
-    ZXTune_GetModuleInfo(plugin->module, &zxinfo);
-
-    plugin->info->numChannels = zxinfo.Channels;
-    plugin->info->numOrders = zxinfo.Positions;
-    plugin->info->loopPosition = zxinfo.LoopPosition;
-    plugin->info->loopFrame = zxinfo.LoopFrame;
-    // plugin->info->numPatterns = zxinfo.Patterns;
-    plugin->info->numFrames = zxinfo.Frames;
-    plugin->info->initialTempo = zxinfo.InitialTempo;
-    //    plugin->info->title = ZXTune_GetInfo(plugin->player,"Title");
-    //    plugin->info->author = ZXTune_GetInfo(plugin->player,"Author");
-    //    plugin->info->replay = ZXTune_GetInfo(plugin->player,"Program");
-    //    plugin->info->comments = ZXTune_GetInfo(plugin->player,"Comment");
-
-    const uint64_t msecDuration = zxinfo.Frames * ZXTune_GetDuration(plugin->player) / 1000;
-    plugin->waveformat.lengthpcm = (msecDuration / 1000.0) * plugin->waveformat.frequency;
-
-    std::string type = ZXTune_GetInfo(plugin->player, "Type");
-
-    if (type == "AS0")
-    {
-        plugin->info->fileformat = "ASC Sound Master v0.xx";
-    }
-    else if (type == "AY")
-    {
-        plugin->info->fileformat = "AY ZX Spectrum/Amstrad CPC";
-    }
-    else if (type == "AYC")
-    {
-        plugin->info->fileformat = "AYC Amstrad CPC";
-    }
-    else if (type == "ASC")
-    {
-        plugin->info->fileformat = "ASC Sound Master v1.xx-2.xx";
-    }
-    else if (type == "ET1")
-    {
-        plugin->info->fileformat = "Extreme Tracker v1.x";
-    }
-    else if (type == "FTC")
-    {
-        plugin->info->fileformat = "Spectrum Fast Tracker";
-    }
-    else if (type == "GTR")
-    {
-        plugin->info->fileformat = "Global Tracker";
-    }
-    else if (type == "MTC")
-    {
-        plugin->info->fileformat = "Multitrack Container";
-    }
-    else if (type == "PSC")
-    {
-        plugin->info->fileformat = "Pro Sound Creator";
-    }
-    else if (type == "PSG")
-    {
-        plugin->info->fileformat = "Spectrum PSG";
-    }
-    else if (type == "PSM")
-    {
-        plugin->info->fileformat = "Pro Sound Maker";
-    }
-    else if (type == "PT1")
-    {
-        plugin->info->fileformat = "Spectrum Pro Tracker 1";
-    }
-    else if (type == "PT2")
-    {
-        plugin->info->fileformat = "Spectrum Pro Tracker 2";
-    }
-    else if (type == "PT3")
-    {
-        plugin->info->fileformat = "Spectrum Pro Tracker 3";
-    }
-    else if (type == "SQT")
-    {
-        plugin->info->fileformat = "SQ Tracker";
-    }
-    else if (type == "ST1")
-    {
-        plugin->info->fileformat = "Spectrum Sound Tracker 1";
-    }
-    else if (type == "ST3")
-    {
-        plugin->info->fileformat = "Spectrum Sound Tracker 3";
-    }
-    else if (type == "STC")
-    {
-        plugin->info->fileformat = "Compiled Spectrum Sound Tracker 1";
-    }
-    else if (type == "STP")
-    {
-        plugin->info->fileformat = "Spectrum Sound Tracker Pro 1";
-    }
-    else if (type == "TXT")
-    {
-        plugin->info->fileformat = "Vortextracker (Pro Tracker 3)";
-    }
-    else if (type == "TS")
-    {
-        plugin->info->fileformat = "TurboSound module for AY Emulator";
-    }
-    else if (type == "VTX")
-    {
-        plugin->info->fileformat = "Vortextracker";
-    }
-    else if (type == "YM") //played by st-sound
-    {
-        plugin->info->fileformat = "YM";
-    }
-    else if (type == "STR")
-    {
-        plugin->info->fileformat = "Sample Tracker";
-    }
-    else if (type == "CHI")
-    {
-        plugin->info->fileformat = "Spectrum Chip Tracker";
-    }
-    else if (type == "SQD")
-    {
-        plugin->info->fileformat = "SG Digital Tracker";
-    }
-    else if (type == "DMM")
-    {
-        plugin->info->fileformat = "Digital Music Maker";
-    }
-    else if (type == "PDT")
-    {
-        plugin->info->fileformat = "Prodigi Tracker";
-    }
-    else if (type == "DST")
-    {
-        plugin->info->fileformat = "Digital Studio for AY and Covox";
-    }
-    else if (type == "COP")
-    {
-        plugin->info->fileformat = "Sam Coupe E-Tracker";
-    }
-    else if (type == "TFE")
-    {
-        plugin->info->fileformat = "TFM Music Maker 1.3+";
-    }
-    else if (type == "TF0")
-    {
-        plugin->info->fileformat = "TFM Music Maker 0.1-1.2";
-    }
-    else if (type == "TFD")
-    {
-        plugin->info->fileformat = "TurboFM Dumped";
-    }
-    else if (type == "TFC")
-    {
-        plugin->info->fileformat = "TurboFM Compiled";
-    }
-    else
-    {
-        plugin->info->fileformat = "Unknown ZXTune";
-        //plugin->info->fileformat =ZXTune_GetInfo(plugin->player,"Program")+"("+type+")";
-    }
-
-    plugin->posAfterSeek = 0;
-    plugin->info->plugin = PLUGIN_zxtune;
-    plugin->info->pluginName = PLUGIN_zxtune_NAME;
-    plugin->info->setSeekable(true);
-
-    return FMOD_OK;
 }
 
 static FMOD_RESULT F_CALL close(FMOD_CODEC_STATE* codec)
 {
-    auto *plugin = static_cast<pluginZxtune *>(codec->plugindata);
+    delete static_cast<pluginZxtune *>(codec->plugindata);
 
-    if (plugin) {
-        ZXTune_DestroyPlayer(plugin->player);
-        ZXTune_CloseModule(plugin->module);
-        ZXTune_CloseData(plugin->data);
-    }
-
-    delete plugin;
     return FMOD_OK;
 }
 
-static FMOD_RESULT F_CALL read(FMOD_CODEC_STATE* codec, void* buffer, unsigned int size, unsigned int* read)
+static FMOD_RESULT F_CALL read(FMOD_CODEC_STATE *codec, void *buffer, unsigned int size, unsigned int *read)
 {
-    auto* plugin = static_cast<pluginZxtune*>(codec->plugindata);
+    auto *plugin = static_cast<pluginZxtune *>(codec->plugindata);
 
-    int samples = ZXTune_RenderSound(plugin->player, buffer, size);
-    if (samples == -1)
-    {
-        return FMOD_ERR_FORMAT;
+    if (plugin->chunkSamplesBuffered == plugin->chunk.size()) {
+        plugin->chunkSamplesBuffered = 0;
+        plugin->chunk = plugin->renderer->Render();
+
+        if (plugin->chunk.empty()) {
+            return FMOD_ERR_FILE_EOF;
+        }
     }
 
-    *read = size;
+    const auto chunkSamplesLeft = static_cast<unsigned int>(plugin->chunk.size()) - plugin->chunkSamplesBuffered;
+    const auto chunkSamplesToBuffer = std::min(chunkSamplesLeft, size);
+
+    std::memcpy(buffer, plugin->chunk.data() + plugin->chunkSamplesBuffered,
+                chunkSamplesToBuffer * sizeof(Sound::Sample));
+
+    plugin->chunkSamplesBuffered += chunkSamplesToBuffer;
+    *read = chunkSamplesToBuffer;
     return FMOD_OK;
 }
 
 static FMOD_RESULT F_CALL setPosition(FMOD_CODEC_STATE* codec, int subsound, unsigned int position, FMOD_TIMEUNIT postype)
 {
-    auto plugin = static_cast<pluginZxtune*>(codec->plugindata);
-    unsigned int pos = (position / 1000) * plugin->waveformat.frequency;
-    plugin->posAfterSeek = ZXTune_SeekSound(plugin->player, pos);
-
-    return FMOD_OK;
-}
-
-//TODO needed?
-static FMOD_RESULT F_CALL getPosition(FMOD_CODEC_STATE* codec, unsigned int* position, FMOD_TIMEUNIT postype)
-{
-    auto* plugin = static_cast<pluginZxtune*>(codec->plugindata);
-    if (postype == FMOD_TIMEUNIT_MS_REAL)
-    {
-        *position = plugin->posAfterSeek;
-    }
+    auto plugin = static_cast<pluginZxtune *>(codec->plugindata);
+    plugin->renderer->SetPosition(Time::Instant<Time::Millisecond>(position));
 
     return FMOD_OK;
 }
