5d2fd88e068939c6dbd4222a16033a51102c0959
Ciro Scognamiglio <ciro.scognamiglio88@gmail.com>
2026-07-23 08:32:24 +0200

    refactor: improve XDG_SESSION_TYPE check

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -17,7 +17,7 @@ public:
 int main(int argc, char *argv[]) {
 #ifndef WIN32
     // TODO temp workaround for qt-ads on wayland https://github.com/githubuser0xFFFF/Qt-Advanced-Docking-System/issues/714
-    if (QString::compare(qEnvironmentVariable("XDG_SESSION_TYPE"), "wayland", Qt::CaseSensitive) == 0) {
+    if (qEnvironmentVariable("XDG_SESSION_TYPE") == "wayland") {
         qputenv("QT_QPA_PLATFORM", "xcb");
     }
 #endif
